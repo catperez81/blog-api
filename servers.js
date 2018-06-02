f16536f48848539bb1330cb0c21886e04da7731d
catperez81 <catperez81@gmail.com>
2018-06-02 16:15:19 -0700

    test file setup

--- servers.js
+++ servers.js
@@ -12,3 +12,5 @@ app.use('/blog-posts', blogPostRouter);
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,38 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+// this lets us use *expect* style syntax in our tests
+// so we can do things like `expect(1 + 1).to.equal(2);`
+// http://chaijs.com/api/bdd/
+const expect = chai.expect;
+
+// This let's us make HTTP requests
+// in our tests.
+// see: https://github.com/chaijs/chai-http
+chai.use(chaiHttp);
+
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    })
+    .on('error', err => {
+      reject(err);
+    });
+  });
+}
+
+//... closeServer defined here
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+
